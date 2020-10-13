8345db0f39e6bb581bdaef39c25232273c25ec57
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 12:56:23 -0400

    Awesome feature 2 added more changes to line 3

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -6,6 +6,7 @@ public class Feature2 {
         System.out.println("Working on Feature2 more ");
         System.out.println("Working on Feature2 again ");
 
+        System.out.println("Adding some more changes to line 3 , virtually");
     }
 
 
