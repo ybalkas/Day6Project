cf45d3c5940ae4e8f0d0a17a4bf0a2b26f829ab6
Yusuf Balkas <ybalkas@gmail.com>
2020-10-13 18:31:16 +0100

    added one more line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Sill really FROM GITHUB MADE SOME CHANGES awesome hello");
-
+        System.out.println("add one more line");
     }
 
 }
