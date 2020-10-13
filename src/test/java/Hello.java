e68111898a9f7dcfdbf69e7a4987eeb054eb4078
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 13:18:39 -0400

    updated the way we say hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Sill awesome hello");
+        System.out.println("Sill really awesome hello");
 
     }
 
