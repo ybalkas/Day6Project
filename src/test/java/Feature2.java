39927cfa305a6dd604e735af09a8a07188cf3cc1
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 12:46:55 -0400

    Awesome feature 2 more added

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -3,6 +3,8 @@ public class Feature2 {
     public static void main(String[] args) {
 
         System.out.println("Working on Feature2 ");
+        System.out.println("Working on Feature2 more ");
+        System.out.println("Working on Feature2 again ");
 
     }
 
